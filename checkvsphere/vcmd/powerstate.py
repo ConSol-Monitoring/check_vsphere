d1fd6d73cad32539a3acaec69392ec27004ecef7
Danijel Tasov <data@consol.de>
2023-05-30 15:25:45 +0000

    add power-state cmd

---
+++ checkvsphere/vcmd/powerstate.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+#    Copyright (C) 2023  ConSol Consulting & Solutions Software GmbH
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+"""
+check power state of all hosts
+"""
+
+__cmd__ = 'power-state'
+
+import logging
+from pyVmomi import vim
+from monplugin import Check, Status, Threshold
+from datetime import datetime, timedelta, timezone
+from ..tools import cli, service_instance
+from ..tools.helper import (
+	CheckArgument,
+	find_entity_views,
+	get_metric,
+    isbanned,
+    isallowed,
+    process_retrieve_content
+)
+
+args = None
+
+def get_argparser():
+    parser = cli.Parser()
+    parser.add_optional_arguments(CheckArgument.BANNED(
+        'regex, checked against <host-name>',
+    ))
+    parser.add_optional_arguments(CheckArgument.ALLOWED(
+        'regex, checked against <host-name>',
+    ))
+    return parser
+
+def run():
+    global args
+    parser = get_argparser()
+    args = parser.get_args()
+
+    check = Check()
+
+    args._si = service_instance.connect(args)
+
+    hosts = find_entity_views(
+        args._si,
+        vim.HostSystem,
+        begin_entity=args._si.content.rootFolder,
+        properties=['name', 'runtime.powerState']
+    )
+    hosts = process_retrieve_content(list(map(lambda x: x['obj'], hosts)))
+
+    total = 0
+    ignored = 0
+    powered = 0
+
+    for host in hosts:
+        total += 1
+        if isbanned(args, f'{host["name"]}'):
+            ignored += 1
+            continue
+        if not isallowed(args, f'{host["name"]}'):
+            ignored += 1
+            continue
+
+        message = f"powerState of { host['name'] } is { host['runtime.powerState']}"
+
+        if host['runtime.powerState'] == 'poweredOn':
+            powered += 1
+            check.add_message(Status.OK, message)
+        else:
+            check.add_message(Status.CRITICAL, message)
+
+    check.add_perfdata(label='hosts', value=total)
+    check.add_perfdata(label='ignored hosts', value=ignored)
+    check.add_perfdata(label='hosts with power', value=powered)
+
+    opt = {}
+    if not args.verbose:
+        opt['allok'] = 'All hosts ok'
+
+    (code, message) = check.check_messages(separator='\n', separator_all='\n', **opt)
+    check.exit(
+        code=code,
+        message=f"{ total } hosts, {ignored} ignored, unpowered { total - ignored - powered }\n" + message,
+    )
